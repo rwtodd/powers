30dff54beef10e278c332a90b411b5b6fcb4a74a
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-03 12:03:48 -0500

    rot13

---
+++ rot13/rot13.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/env lua
+function rot13(s)
+	return (s:gsub('%a',
+		function(c)
+			c = c:byte()
+			return string.char(c+(c%32<14 and 13 or -13))
+		end))
+end
+
+for l in io.lines() do print(rot13(l)) end
