c2d65ef84eb681f36f9e4f4457d0c26cb373047f
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-26 15:10:15 -0500

    added the %X and %. options from ddate

--- date/check.lua
+++ date/check.lua
@@ -5,4 +5,19 @@ assert(tbl.Y == '3186')
 assert(tbl.H == 'Chaoflux')
 assert(tbl.B == 'Chaos')
 assert(tbl.a == 'SO')
+
+-- spot check X-Day calculation
+assert(d.date('%X',os.time{year=8660,month=2,day=29})=='492') -- "LAST TIBS EVER"
+assert(d.date('%X',os.time{year=8656,month=2,day=29})=='1953') -- "2nd-to-last LAST TIBS EVER"
+assert(d.date('%X',os.time{year=8661,month=2,day=1})=='154')
+assert(d.date('%X',os.time{year=8661,month=7,day=1})=='4')
+assert(d.date('%X',os.time{year=8661,month=7,day=5})=='0')
+assert(d.date('%X',os.time{year=8661,month=8,day=1})=='-1')
+assert(d.date('%X',{year=8661,yday=1})=='185')
+assert(d.date('%X',{year=8651,yday=1})=='3838')
+assert(d.date('%X',{year=8641,yday=1})=='7490')
+assert(d.date('%X',{year=8631,yday=1})=='11143')
+assert(d.date('%X',{year=8621,yday=1})=='14795')
+assert(d.date('%X',{year=8611,yday=1})=='18448')
+assert(d.date('%X',{year=8601,yday=1})=='22100')
 print('OK!')
