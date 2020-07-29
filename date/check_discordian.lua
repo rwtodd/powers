f1eda91bd588abcac7cb5ded5a2a1795c435993e
Richard Todd <rwtodd@users.noreply.github.com>
2020-07-29 18:14:40 -0500

    made the %X calculation loopless for leap years

--- date/check_discordian.lua
+++ date/check_discordian.lua
@@ -23,4 +23,25 @@ assert(d.date('%X',os.time{year=8660,month=3,day=1})=='491')
 assert(d.date('%X',os.time{year=8660,month=2,day=29})=='492') -- "LAST TIBS EVER"
 assert(d.date('%X',os.time{year=8660,month=2,day=28})=='493')
 assert(d.date('%X',os.time{year=8656,month=2,day=29})=='1953') -- "2nd-to-last LAST TIBS"
+
+-- Now check a smattering of years...
+local function leap_p(greg_year)
+	return (greg_year%4==0) and (greg_year%100~=0 or greg_year%400==0)
+end
+
+local dt = { year=8661,yday=186 }
+local cnt = 0
+while dt.year > 5999 do
+	while dt.yday > 0 do
+		local calculated = tonumber(d.date("%X",dt))
+		assert(calculated==cnt, 
+			string.format("year %d day %d (%d vs correct %d)",
+				dt.year, dt.yday, calculated, cnt))
+		dt.yday = dt.yday - 1
+		cnt = cnt + 1
+	end	
+	dt.year = dt.year - 1
+	dt.yday = 365 + (leap_p(dt.year) and 1 or 0)
+end
+
 print('date.discordian... OK!')
